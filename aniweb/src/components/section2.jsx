ff1481ec0044a4aa6cd7a63a1b7aa6ae379fe326
Abdalrhman mohamed <abdalrhmanhmouda@gmail.com>
2025-11-06 22:25:42 +0200

    section3

--- aniweb/src/components/section2.jsx
+++ aniweb/src/components/section2.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import "./section2.css"
 import business from "../assets/business.svg"
+import mag from "../assets/mag.svg"
 const Business = () => {
     return ( <>
     <img className='bus' src= {business}  />
+    <img className='mag' src={mag} />
     </> );
 }
  
