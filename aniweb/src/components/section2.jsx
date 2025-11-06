be7737d8016e4762cd1cbb94ad72b62899962538
Abdalrhman mohamed <abdalrhmanhmouda@gmail.com>
2025-11-06 22:37:42 +0200

    footer

--- aniweb/src/components/section2.jsx
+++ aniweb/src/components/section2.jsx
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import "./section2.css"
 import business from "../assets/business.svg"
 import mag from "../assets/mag.svg"
+import opti from "../assets/optimal.svg"
+import info from "../assets/info.svg"
 const Business = () => {
     return ( <>
     <img className='bus' src= {business}  />
     <img className='mag' src={mag} />
+    <img className='opti' src={opti} />
+    <img className='info' src={info} />
     </> );
 }
  
