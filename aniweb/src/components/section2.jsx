06143f5d05e45b0fd674ee045079a4f3b0379bbe
Abdalrhman mohamed <abdalrhmanhmouda@gmail.com>
2025-11-06 22:18:48 +0200

    section2

---
+++ aniweb/src/components/section2.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import "./section2.css"
+import business from "../assets/business.svg"
+const Business = () => {
+    return ( <>
+    <img className='bus' src= {business}  />
+    </> );
+}
+ 
+export default Business;
--- aniweb/src/pages/Home.jsx
+++ aniweb/src/pages/Home.jsx
@@ -3,10 +3,12 @@ import "./Home.css"
 import Head from '../components/Head';
 import Heads from '../components/Head';
 import Sec from '../components/Section';
+import Business from '../components/section2';
 const Home = () => {
     return ( <>
     <Heads />
     <Sec />
+    <Business />
     </>  );
 }
  
